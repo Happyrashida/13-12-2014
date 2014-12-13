310a775232a0635bdd5d9ce57c232af8f1cb15bc
Happyrashida <happyrashida@gmail.com>
2014-12-13 14:06:39 +0600

    New file added

---
+++ CitizenInformationApp/CitizenInformationApp/DictionaryUI.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace CitizenInformationApp
+{
+    partial class DictionaryUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.idAddTextBox = new System.Windows.Forms.TextBox();
+            this.detailsAddRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.idSearchTextBox = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonShowAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Details";
+            // 
+            // idAddTextBox
+            // 
+            this.idAddTextBox.Location = new System.Drawing.Point(103, 13);
+            this.idAddTextBox.Name = "idAddTextBox";
+            this.idAddTextBox.Size = new System.Drawing.Size(174, 20);
+            this.idAddTextBox.TabIndex = 2;
+            // 
+            // detailsAddRichTextBox
+            // 
+            this.detailsAddRichTextBox.Location = new System.Drawing.Point(103, 40);
+            this.detailsAddRichTextBox.Name = "detailsAddRichTextBox";
+            this.detailsAddRichTextBox.Size = new System.Drawing.Size(174, 63);
+            this.detailsAddRichTextBox.TabIndex = 3;
+            this.detailsAddRichTextBox.Text = "";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(293, 79);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 4;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Id";
+            // 
+            // idSearchTextBox
+            // 
+            this.idSearchTextBox.Location = new System.Drawing.Point(103, 144);
+            this.idSearchTextBox.Name = "idSearchTextBox";
+            this.idSearchTextBox.Size = new System.Drawing.Size(174, 20);
+            this.idSearchTextBox.TabIndex = 6;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(293, 142);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 7;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // buttonShowAll
+            // 
+            this.buttonShowAll.Location = new System.Drawing.Point(148, 190);
+            this.buttonShowAll.Name = "buttonShowAll";
+            this.buttonShowAll.Size = new System.Drawing.Size(99, 47);
+            this.buttonShowAll.TabIndex = 8;
+            this.buttonShowAll.Text = "Show All";
+            this.buttonShowAll.UseVisualStyleBackColor = true;
+            this.buttonShowAll.Click += new System.EventHandler(this.buttonShowAll_Click);
+            // 
+            // DictionaryUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 261);
+            this.Controls.Add(this.buttonShowAll);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.idSearchTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.detailsAddRichTextBox);
+            this.Controls.Add(this.idAddTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "DictionaryUI";
+            this.Text = "DictionaryUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox idAddTextBox;
+        private System.Windows.Forms.RichTextBox detailsAddRichTextBox;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox idSearchTextBox;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonShowAll;
+    }
+}
---
+++ CitizenInformationApp/CitizenInformationApp/DictionaryUI.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CitizenInformationApp
+{
+    public partial class DictionaryUI : Form
+    {
+        public DictionaryUI()
+        {
+            InitializeComponent();
+        }
+
+        Dictionary<int, string> citizenDictionary = new Dictionary<int, string>(); 
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            if (citizenDictionary.ContainsKey(Convert.ToInt32(idSearchTextBox.Text)))
+            {
+                int id = Convert.ToInt16(idSearchTextBox.Text);
+                MessageBox.Show(citizenDictionary[id]);
+                idSearchTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("ID not in list!!!");
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (citizenDictionary.ContainsKey(Convert.ToInt32(idAddTextBox.Text)))
+            {
+                MessageBox.Show("ID already exists!!!");
+            }
+            else
+            {
+                int id = Convert.ToInt16(idAddTextBox.Text);
+                string details = detailsAddRichTextBox.Text;
+                citizenDictionary.Add(id, details);
+                idAddTextBox.Clear();
+                detailsAddRichTextBox.Clear();
+            }
+        }
+
+        private void buttonShowAll_Click(object sender, EventArgs e)
+
+        {
+            string showAllInfo = string.Empty;
+            foreach (var VARIABLE in citizenDictionary)
+            {
+                showAllInfo += VARIABLE.Key + " " + VARIABLE.Value + "\n";
+            }
+            MessageBox.Show(showAllInfo);
+        }
+    }
+}
